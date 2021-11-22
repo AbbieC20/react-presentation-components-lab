dbb09c8871ca64afe81f9d4223e059c8533f0f5c
Abbie Cornelius <abbyc@sky.com>
2021-11-22 20:43:16 +0000

    commit tree 4ac2734974603cf59212ed711e95d28c2e9d242a

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={() => alert("Your mood is....")} />
   </div>,
   document.getElementById('root')
 );
